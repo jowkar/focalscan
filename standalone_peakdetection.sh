4ac3beb1e31a0c8d71d27dabb5c69680f2eefd40
jowkar <j.w.karlsson@gmail.com>
2016-04-28 20:13:27 +0200

    latest

--- standalone_peakdetection.sh
+++ standalone_peakdetection.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
-argstring=`./build_argstring.sh $@`
+# Find installation directory
+install_dir=`echo $PATH | tr : '\n' | grep focalscan`
+if [ -z "$install_dir" ]
+then
+    echo "The installation directory was not found in the PATH variable. Checking if the currect directory contains the necessary files"
+    main_file=`ls | grep FocalScan.m`
+    echo $main_file
+    if [ -z "$main_file" ]
+    then
+        echo "The necessary files were not found in the current directory. Please manually add the directory countaining the FocalScan files to PATH by typing: export PATH=\$PATH:path_to/focalscan"
+        exit $?
+    else
+        echo "Main file found. Adding current directory to PATH"
+        export PATH=$PATH:`pwd`
+    fi
+    install_dir=`echo $PATH | tr : '\n' | grep focalscan`
+    if [ -z "$install_dir" ]
+    then
+        echo "Failed adding current directory to PATH, for unknown reasons"
+        exit $?
+    fi
+fi
 
-matlab -nodesktop -nosplash -r "try; standalone_peakdetection($argstring); catch ME; disp(getReport(ME,'extended','hyperlinks','off')); exit; end; exit"
+current_dir=`pwd`
+
+argstring=`$install_dir/build_argstring.sh $@`
+#argstring=`build_argstring.sh $@`
+
+matlab -nodesktop -nosplash -r "addpath(genpath('$install_dir'));cd('$current_dir');try; standalone_peakdetection($argstring); catch ME; disp(getReport(ME,'extended','hyperlinks','off')); exit; end; exit"
