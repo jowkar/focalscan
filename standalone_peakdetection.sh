7e4781992340e15fc9b3e0b2819d238963edb4be
Joakim Karlsson <j.w.karlsson@gmail.com>
2016-05-05 22:01:10 +0200

    Adressed some portability issues

--- standalone_peakdetection.sh
+++ standalone_peakdetection.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 # Find installation directory
-install_dir=`echo $PATH | tr : '\n' | grep focalscan`
+install_dir=$(echo "$PATH" | tr : '\n' | grep focalscan)
 if [ -z "$install_dir" ]
 then
     echo "The installation directory was not found in the PATH variable. Checking if the currect directory contains the necessary files"
-    main_file=`ls | grep FocalScan.m`
-    echo $main_file
-    if [ -z "$main_file" ]
+    if [ -f "./FocalScan.m" ];
     then
+        echo "Main file found. Temporarily adding current directory to PATH"
+        export PATH="$PATH":"$(pwd)"
+    else
         echo "The necessary files were not found in the current directory. Please manually add the directory countaining the FocalScan files to PATH by typing: export PATH=\$PATH:path_to/focalscan"
         exit $?
-    else
-        echo "Main file found. Adding current directory to PATH"
-        export PATH=$PATH:`pwd`
     fi
-    install_dir=`echo $PATH | tr : '\n' | grep focalscan`
+
+    install_dir=$(echo "$PATH" | tr : '\n' | grep focalscan)
     if [ -z "$install_dir" ]
     then
         echo "Failed adding current directory to PATH, for unknown reasons"
@@ -23,30 +22,30 @@ then
     fi
 fi
 
-current_dir=`pwd`
+current_dir=$(pwd)
 
-argstring=`$install_dir/build_argstring.sh $@`
-#argstring=`build_argstring.sh $@`
+argstring=$("$install_dir"/build_argstring.sh "$@")
+#argstring=$(build_argstring.sh $@)
 
 if [ -z "$argstring" ]
 then
     echo
-    echo "`tput bold`USAGE:`tput sgr0`
+    echo "$(tput bold)USAGE:$(tput sgr0)
     standalone_peakdetection.sh report_file_path annot_file_path peak_level scorefield out_file
 
-        `tput bold`report_file_path:`tput sgr0` path to main report file created by running FocalScan (ie. report.txt)
-        `tput bold`annot_file_path:`tput sgr0` path to annotation file (ie. annotation/gencode17.bed, gene/tile ids must match those in the report file)
-        `tput bold`peak_level:`tput sgr0` level of granularity at which to find peaks (0.1-1, where 1 is least granular)
-        `tput bold`scorefield:`tput sgr0` metric in the report file to be used as basis for peak detection;
+        $(tput bold)report_file_path:$(tput sgr0) path to main report file created by running FocalScan (ie. report.txt)
+        $(tput bold)annot_file_path:$(tput sgr0) path to annotation file (ie. annotation/gencode17.bed, gene/tile ids must match those in the report file)
+        $(tput bold)peak_level:$(tput sgr0) level of granularity at which to find peaks (0.1-1, where 1 is least granular)
+        $(tput bold)scorefield:$(tput sgr0) metric in the report file to be used as basis for peak detection;
             (Valid options:
-                `tput smul`fs_hp:`tput rmul` the standard FocalScan score (with focality filter)
-                `tput smul`fs:`tput rmul` FocalScan score without focality filter
-                `tput smul`sum_cna_hp:`tput rmul` summed copy number amplitudes, with focality filter 
-                `tput smul`sum_cna:`tput rmul` summed copy number amplitudes, without focality filter 
-                `tput smul`spearman_corr:`tput rmul` spearman correlation coefficient)
-        `tput bold`out_file:`tput sgr0` name of output file (ie. peaks.txt)"
+                $(tput smul)fs_hp:$(tput rmul) the standard FocalScan score (with focality filter)
+                $(tput smul)fs:$(tput rmul) FocalScan score without focality filter
+                $(tput smul)sum_cna_hp:$(tput rmul) summed copy number amplitudes, with focality filter 
+                $(tput smul)sum_cna:$(tput rmul) summed copy number amplitudes, without focality filter 
+                $(tput smul)spearman_corr:$(tput rmul) spearman correlation coefficient)
+        $(tput bold)out_file:$(tput sgr0) name of output file (ie. peaks.txt)"
     echo
-    echo "`tput bold`EXAMPLE`tput sgr0`
+    echo "$(tput bold)EXAMPLE$(tput sgr0)
     standalone_peakdetection.sh report.txt annotation/gencode17.bed 0.7 fs_hp ./peaks.txt"
     echo
 else
