0a7b21e61a944be361d10a3353a5fcb41a2b4eae
jowkar <j.w.karlsson@gmail.com>
2016-05-05 20:59:57 +0200

    Added help message to standalone_peakdetection.sh

--- standalone_peakdetection.sh
+++ standalone_peakdetection.sh
@@ -28,4 +28,27 @@ current_dir=`pwd`
 argstring=`$install_dir/build_argstring.sh $@`
 #argstring=`build_argstring.sh $@`
 
-matlab -nodesktop -nosplash -r "addpath(genpath('$install_dir'));cd('$current_dir');try; standalone_peakdetection($argstring); catch ME; disp(getReport(ME,'extended','hyperlinks','off')); exit; end; exit"
+if [ -z "$argstring" ]
+then
+    echo
+    echo "`tput bold`USAGE:`tput sgr0`
+    standalone_peakdetection.sh report_file_path annot_file_path peak_level scorefield out_file
+
+        `tput bold`report_file_path:`tput sgr0` path to main report file created by running FocalScan (ie. report.txt)
+        `tput bold`annot_file_path:`tput sgr0` path to annotation file (ie. annotation/gencode17.bed, gene/tile ids must match those in the report file)
+        `tput bold`peak_level:`tput sgr0` level of granularity at which to find peaks (0.1-1, where 1 is least granular)
+        `tput bold`scorefield:`tput sgr0` metric in the report file to be used as basis for peak detection;
+            (Valid options:
+                `tput smul`fs_hp:`tput rmul` the standard FocalScan score (with focality filter)
+                `tput smul`fs:`tput rmul` FocalScan score without focality filter
+                `tput smul`sum_cna_hp:`tput rmul` summed copy number amplitudes, with focality filter 
+                `tput smul`sum_cna:`tput rmul` summed copy number amplitudes, without focality filter 
+                `tput smul`spearman_corr:`tput rmul` spearman correlation coefficient)
+        `tput bold`out_file:`tput sgr0` name of output file (ie. peaks.txt)"
+    echo
+    echo "`tput bold`EXAMPLE`tput sgr0`
+    standalone_peakdetection.sh report.txt annotation/gencode17.bed 0.7 fs_hp ./peaks.txt"
+    echo
+else
+    matlab -nodesktop -nosplash -r "addpath(genpath('$install_dir'));cd('$current_dir');try; standalone_peakdetection($argstring); catch ME; disp(getReport(ME,'extended','hyperlinks','off')); exit; end; exit"
+fi
